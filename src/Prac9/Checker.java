446972389f9b783d69cf644f54f47e57d1ee3902
Mikhail <mr1302@ya.ru>
2021-10-29 02:27:33 +0300

    Made Prac9
    Also added idea project files

---
+++ src/Prac9/Checker.java
@@ -0,0 +1,30 @@
+package Prac9;
+
+public class Checker {
+    String fullName = "";
+    String INN = "";
+
+    public Checker(String fullName, String INN) throws InvalidINNException {
+        if(checkINN(INN))
+            this.INN = INN;
+        this.fullName = fullName;
+    }
+
+    public Checker(String fullName) {
+        this.fullName = fullName;
+    }
+
+    boolean checkINN(String INN) throws InvalidINNException{
+        if(INN.length() != 10 && INN.length() != 12)
+            throw new InvalidINNException("Wrong INN length");
+        for(int i = 0; i < INN.length(); ++i)
+            if(INN.charAt(i) < '0' || INN.charAt(i) > '9')
+                throw new InvalidINNException("Wrong INN character");
+        return true;
+    }
+
+    void enterINN(String INN) throws InvalidINNException{
+        if(checkINN(INN))
+            this.INN = INN;
+    }
+}
---
+++ src/Prac9/InvalidINNException.java
@@ -0,0 +1,7 @@
+package Prac9;
+
+public class InvalidINNException extends RuntimeException{
+    public InvalidINNException(String message) {
+        super(message);
+    }
+}
